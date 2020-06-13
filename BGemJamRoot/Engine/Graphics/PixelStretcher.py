dc15492e085a6767c455adac33c1eb2efa6c675a
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-12 17:40:18 -0700

    PixelStretcher.py
    The current script for changing resolutions. It is currently just a template.

---
+++ BGemJamRoot/Engine/Graphics/PixelStretcher.py
@@ -0,0 +1,10 @@
+# Start of script
+"""
+The pixel stretcher script is the games engine on stretching and un-stretching the graphics so the game can easily switch between a low resolution (example: 256x144) to a high resolution (example: 30720x17280) and vice versa with little issue (other than a little bit of lag while switching of course, it won't be perfect on the first try)
+This script is still highly incomplete.
+"""
+# Run
+print("This script is currently unusable. Please close the program")
+noMoreExit = input("Press [ENTER] key to quit")
+print("The program has been closed. If this window is still open, press the close button or perform a -taskkill on this process")
+# End of script
