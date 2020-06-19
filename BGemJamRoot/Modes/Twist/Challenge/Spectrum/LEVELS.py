139aadb2da4683554329a4b7d3a5006afc1193ba
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-18 18:05:16 -0700

    LEVELS.py
    The current 10 levels for Spectrum mode.

---
+++ BGemJamRoot/Modes/Twist/Challenge/Spectrum/LEVELS.py
@@ -0,0 +1,81 @@
+# Start of script
+"""
+Spectrum mode
+
+Spectrum is a challenge planet for challenge mode, with the objective of destroying a specific color of gems consecutively
+Unlike the Bejeweled Twist challenge mode, there are more than 7 levels per challenge, and levels are replayable
+"""
+# Levels
+''' Level 1 '''
+def spectrumLvl1(): # Spectrum challenge level 1
+  spectrumlv1bo = bool(false) # Unlock status
+  spectrumlv1lv = int(1) # Level value
+  spectrumlv1objective = str("NULL - Need to get the data from the original game")
+''' Level 2 '''
+def spectrumLvl2(): # Spectrum challenge level 2
+  spectrumlv2bo = bool(false) # Unlock status
+  spectrumlv2lv = int(2) # Level value
+  spectrumlv2objective = str("NULL - Need to get the data from the original game")
+''' Level 3 '''
+def spectrumLvl3(): # Spectrum challenge level 3
+  spectrumlv3bo = bool(false) # Unlock status
+  spectrumlv3lv = int(3) # Level value
+  spectrumlv3objective = str("NULL - Need to get the data from the original game")
+''' Level 4 '''
+def spectrumLvl4(): # Spectrum challenge level 4
+  spectrumlv4bo = bool(false) # Unlock status
+  spectrumlv4lv = int(4) # Level value
+  spectrumlv4objective = str("NULL - Need to get the data from the original game")
+''' Level 5 '''
+def spectrumLvl5(): # Spectrum challenge level 5
+  spectrumlv5bo = bool(false) # Unlock status
+  spectrumlv5lv = int(5) # Level value
+  spectrumlv5objective = str("NULL - Need to get the data from the original game")
+''' Level 6 '''
+def spectrumLvl6(): # Spectrum challenge level 6
+  spectrumlv6bo = bool(false) # Unlock status
+  spectrumlv6lv = int(6) # Level value
+  spectrumlv6objective = str("NULL - Need to get the data from the original game")
+''' Level 7 '''
+def spectrumLvl7(): # Spectrum challenge level 7
+  spectrumlv7bo = bool(false) # Unlock status
+  spectrumlv7lv = int(7) # Level value
+  spectrumlv7objective = str("NULL - Need to get the data from the original game")
+"""
+DATA IMPORT
+Spectrum 1 	Make 3 red matches in a row
+Spectrum 2 	Make 4 orange matches in a row
+Spectrum 3 	Make 5 yellow matches in a row
+Spectrum 4 	Make 6 green matches in a row
+Spectrum 5 	Make 7 blue matches in a row
+Spectrum 6 	Make 8 white matches in a row
+Spectrum 7 	Make 9 violet matches in a row
+Spectrum Eclipse 	Make as many violet matches in a row as you can in 3 minutes
+"""
+''' Level 8 '''
+def spectrumLvl8(): # Spectrum challenge level 8
+  spectrumlv8bo = bool(false) # Unlock status
+  spectrumlv8lv = int(8) # Level value
+  spectrumlv8objective = str("Spectrum VIII\nMake 2 red matches in a row, followed up by 2 orange matches in a row") # Objective
+''' Level 9 '''
+def spectrumLvl9(): # Spectrum challenge level 9
+  spectrumlv9bo = bool(false) # Unlock status
+  spectrumlv9lv = int(9) # Level value
+  spectrumlv9objective = str("Spectrum IX\nMake 2 red matches in a row, followed up by 2 orange matches in a row, followed up by 2 yellow matches in a row") # Objective
+def spectrumLvl10(): # Spectrum challenge level 9
+  spectrumlv10bo = bool(false) # Unlock status
+  spectrumlv10lv = int(10) # Level value
+  spectrumlv10objective = str("Spectrum X\nMake 4 red matches in a row") # Objective
+# Stopping at 10 levels for now
+"""
+Idea:
+Match every gem color x times each in a row
+3-10 matches for every color in a row separately
+"""
+""" Divider """
+# Accidental/Illegal direct launch
+print("Challenge island #[unknown] - Spectrum")
+noMore = input("This file isn't meant to be executed separately press [ENTER] to close it")
+print("The program has been stopped. If the window is still open, press the close button, or try the --taskkill command")
+""" Divider """
+# End of script
