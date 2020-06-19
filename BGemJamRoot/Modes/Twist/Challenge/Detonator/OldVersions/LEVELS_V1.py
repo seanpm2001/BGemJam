318d18602b7386242ef88732202ee7715a24aca0
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-18 17:50:09 -0700

    LEVELS_V1.py
    An archive of the first 10 levels of the Detonator challenge mode.

---
+++ BGemJamRoot/Modes/Twist/Challenge/Detonator/OldVersions/LEVELS_V1.py
@@ -0,0 +1,76 @@
+# Start of script
+"""
+Detonator mode
+
+Detonator is a challenge planet for challenge mode, with the objective of destroying a set amount of gems in 1 move.
+Unlike the Bejeweled Twist challenge mode, there are more than 7 levels per challenge, and levels are replayable
+"""
+# Levels
+''' Level 1 '''
+def detonatorLvl1(): # Detonator challenge level 1
+  detolv1bo = bool(false) # Unlock status
+  detolv1lv = int(1) # Level value
+  detolv1objective = str("NULL - Need to get the data from the original game")
+''' Level 2 '''
+def detonatorLvl2(): # Detonator challenge level 2
+  detolv2bo = bool(false) # Unlock status
+  detolv2lv = int(2) # Level value
+  detolv2objective = str("NULL - Need to get the data from the original game")
+''' Level 3 '''
+def detonatorLvl3(): # Detonator challenge level 3
+  detolv3bo = bool(false) # Unlock status
+  detolv3lv = int(3) # Level value
+  detolv3objective = str("NULL - Need to get the data from the original game")
+''' Level 4 '''
+def detonatorLvl4(): # Detonator challenge level 4
+  detolv4bo = bool(false) # Unlock status
+  detolv4lv = int(4) # Level value
+  detolv4objective = str("NULL - Need to get the data from the original game")
+''' Level 5 '''
+def detonatorLvl5(): # Detonator challenge level 5
+  detolv5bo = bool(false) # Unlock status
+  detolv5lv = int(5) # Level value
+  detolv5objective = str("NULL - Need to get the data from the original game")
+''' Level 6 '''
+def detonatorLvl6(): # Detonator challenge level 6
+  detolv6bo = bool(false) # Unlock status
+  detolv6lv = int(6) # Level value
+  detolv6objective = str("NULL - Need to get the data from the original game")
+''' Level 7 '''
+def detonatorLvl7(): # Detonator challenge level 7
+  detolv7bo = bool(false) # Unlock status
+  detolv7lv = int(7) # Level value
+  detolv7objective = str("NULL - Need to get the data from the original game")
+"""
+DATA IMPORT
+Detonator 1   Destroy 8 gems in 1 move
+Detonator 2 	Destroy 12 gems in 1 move without using any Lightning
+Detonator 3 	Destroy 15 gems in 1 move
+Detonator 4 	Destroy 18 gems in 1 move without using any Lightning
+Detonator 5 	Destroy 25 gems in 1 move
+Detonator 6 	Destroy 35 gems in 1 move
+Detonator 7 	Destroy all gems on board in 1 move
+Detonator Eclipse 	Get the highest single move score possible in 3 minutes
+"""
+''' Level 8 '''
+def detonatorLvl8(): # Detonator challenge level 8
+  detolv8bo = bool(false) # Unlock status
+  detolv8lv = int(8) # Level value
+  detolv8objective = str("Detonator VIII\nDestroy 100 gems in move") # Objective
+''' Level 9 '''
+def detonatorLvl9(): # Detonator challenge level 9
+  detolv9bo = bool(false) # Unlock status
+  detolv9lv = int(9) # Level value
+  detolv9objective = str("Detonator IX\nDestroy 110 gems in move") # Objective
+def detonatorLvl10(): # Detonator challenge level 9
+  detolv10bo = bool(false) # Unlock status
+  detolv10lv = int(10) # Level value
+  detolv10objective = str("Detonator X\nDestroy 125 gems in move") # Objective
+# Stopping at 10 levels for now
+""" Divider """
+# Accidental/Illegal direct launch
+print("Challenge island #[unknown] - Detonator")
+noMore = input("This file isn't meant to be executed separately press [ENTER] to close it")
+print("The program has been stopped. If the window is still open, press the close button, or try the --taskkill command")
+""" Divider """
+# End of script
