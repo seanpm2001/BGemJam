89da0f25600c1cdfda990aa27c984ad119278847
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-18 17:15:06 -0700

    LEVELS.py
    A list of the first 25 levels of the locktwist challenge.

---
+++ BGemJamRoot/Modes/Twist/Challenge/Locktwist/LEVELS.py
@@ -0,0 +1,142 @@
+# Start of script
+"""
+Locktwist mode
+
+LockTwist is a challenge planet for challenge mode, with the objective of matching locked gems. It was not a part of Bejeweled Twist, I came up with the idea while playing challenge mode.
+
+Unlike the Bejeweled Twist challenge mode, there are more than 7 levels per challenge, and levels are replayable
+"""
+# Levels
+''' Level 1 '''
+def lockTwistLvl1(): # Locktwist challenge level 1
+  ltlv1bo = bool(false) # Unlock status
+  ltlv1lv = int(1) # Level value
+  ltlv1objective = str("Locktwist I\nUnlock 3 gems") # Objective
+''' Level 2 '''
+def lockTwistLvl2(): # Locktwist challenge level 2
+  ltlv2bo = bool(false) # Unlock status
+  ltlv2lv = int(2) # Level value
+  ltlv2objective = str("Locktwist II\nUnlock 5 gems in 5 minutes") # Level objective
+''' Level 3 '''
+def lockTwistLvl3(): # Locktwist challenge level 3
+  ltlv3bo = bool(false) # Unlock status
+  ltlv3lv = int(3) # Level value
+  ltlv3objective = str("Locktwist III\nUnlock 12 gems") # Level objective
+''' Level 4 '''
+def lockTwistLvl4(): # Locktwist challenge level 4
+  ltlv4bo = bool(false) # Unlock status
+  ltlv4lv = int(4) # Level value
+  ltlv4objective = str("Locktwist IV\nUnlock 20 gems") # Level objective
+''' Level 5 '''
+def lockTwistLvl5(): # Locktwist challenge level 5
+  ltlv5bo = bool(false) # Unlock status
+  ltlv5lv = int(5) # Level value
+  ltlv5objective = str("Locktwist V\nUnlock 35 gems") # Level objective
+''' Level 6 '''
+def lockTwistLvl6(): # Locktwist challenge level 6
+  ltlv6bo = bool(false) # Unlock status
+  ltlv6lv = int(6) # Level value
+  ltlv6objective = str("Locktwist VI\nUnlock 50 gems") # Level objective
+''' Level 7 '''
+def lockTwistLvl7(): # Locktwist challenge level 7
+  ltlv7bo = bool(false) # Unlock status
+  ltlv7lv = int(7) # Level value
+  ltlv7objective = str("Locktwist VII\nUnlock 75 gems") # Level objective
+''' Level 8 '''
+def lockTwistLvl8(): # Locktwist challenge level 8
+  ltlv8bo = bool(false) # Unlock status
+  ltlv8lv = int(8) # Level value
+  ltlv8objective = str("Locktwist VIII\nUnlock 100 gems") # Level objective
+''' Level 9 '''
+def lockTwistLvl9(): # Locktwist challenge level 9
+  ltlv9bo = bool(false) # Unlock status
+  ltlv9lv = int(9) # Level value
+  ltlv9objective = str("Locktwist IX\nUnlock 150 gems") # Level objective
+''' Level 10 '''
+def lockTwistLvl10(): # Locktwist challenge level 10
+  ltlv10bo = bool(false) # Unlock status
+  ltlv10lv = int(10) # Level value
+  ltlv10objective = str("Locktwist X\nUnlock 200 gems") # Level objective
+''' Level 11 '''
+def lockTwistLvl11(): # Locktwist challenge level 11
+  ltlv11bo = bool(false) # Unlock status
+  ltlv11lv = int(11) # Level value
+  ltlv11objective = str("Locktwist XI\nUnlock 350 gems") # Level objective
+''' Level 12 '''
+def lockTwistLvl12(): # Locktwist challenge level 12
+  ltlv12bo = bool(false) # Unlock status
+  ltlv12lv = int(12) # Level value
+  ltlv12objective = str("Locktwist XII\nUnlock 500 gems") # Level objective
+''' Level 13 '''
+def lockTwistLvl13(): # Locktwist challenge level 13
+  ltlv13bo = bool(false) # Unlock status
+  ltlv13lv = int(13) # Level value
+  ltlv13objective = str("Locktwist XIII\nUnlock 600 gems") # Level objective
+''' Level 14 '''
+def lockTwistLvl14(): # Locktwist challenge level 14
+  ltlv14bo = bool(false) # Unlock status
+  ltlv14lv = int(14) # Level value
+  ltlv14objective = str("Locktwist XIV\nUnlock 750 gems") # Level objective
+''' Level 15 '''
+def lockTwistLvl15(): # Locktwist challenge level 15
+  ltlv15bo = bool(false) # Unlock status
+  ltlv15lv = int(15) # Level value
+  ltlv15objective = str("Locktwist XV\nUnlock 850 gems") # Level objective
+''' Level 16 '''
+def lockTwistLvl16(): # Locktwist challenge level 16
+  ltlv16bo = bool(false) # Unlock status
+  ltlv16lv = int(16) # Level value
+  ltlv16objective = str("Locktwist XVI\nUnlock 1000 gems") # Level objective
+''' Level 17 '''
+def lockTwistLvl17(): # Locktwist challenge level 17
+  ltlv17bo = bool(false) # Unlock status
+  ltlv17lv = int(17) # Level value
+  ltlv17objective = str("Locktwist XVII\nUnlock 1250 gems") # Level objective
+''' Level 18 '''
+def lockTwistLvl18(): # Locktwist challenge level 18
+  ltlv18bo = bool(false) # Unlock status
+  ltlv18lv = int(18) # Level value
+  ltlv18objective = str("Locktwist XVIII\nUnlock 1600 gems") # Level objective
+''' Level 19 '''
+def lockTwistLvl19(): # Locktwist challenge level 19
+  ltlv19bo = bool(false) # Unlock status
+  ltlv19lv = int(19) # Level value
+  ltlv19objective = str("Locktwist XIX\nUnlock 1800 gems") # Level objective
+''' Level 20 '''
+def lockTwistLvl20(): # Locktwist challenge level 20
+  ltlv20bo = bool(false) # Unlock status
+  ltlv20lv = int(20) # Level value
+  ltlv20objective = str("Locktwist XX\nUnlock 2000 gems") # Level objective
+''' Level 21 '''
+def lockTwistLvl21(): # Locktwist challenge level 21
+  ltlv21bo = bool(false) # Unlock status
+  ltlv21lv = int(21) # Level value
+  ltlv21objective = str("Locktwist XXI\nUnlock a doom gem before it is activated") # Level objective
+''' Level 22 '''
+def lockTwistLvl22(): # Locktwist challenge level 22
+  ltlv22bo = bool(false) # Unlock status
+  ltlv22lv = int(22) # Level value
+  ltlv22objective = str("Locktwist XXII\nUnlock a doom gem before it is activated in 3 minutes") # Level objective
+''' Level 23 '''
+def lockTwistLvl23(): # Locktwist challenge level 23
+  ltlv23bo = bool(false) # Unlock status
+  ltlv23lv = int(23) # Level value
+  ltlv23objective = str("Locktwist XXIII\nUnlock 2500 gems") # Level objective
+''' Level 24 '''
+def lockTwistLvl24(): # Locktwist challenge level 24
+  ltlv24bo = bool(false) # Unlock status
+  ltlv24lv = int(24) # Level value
+  ltlv24objective = str("Locktwist XXIV\nUnlock 3200 gems") # Level objective
+''' Level 25 '''
+def lockTwistLvl25(): # Locktwist challenge level 25
+  ltlv25bo = bool(false) # Unlock status
+  ltlv25lv = int(25) # Level value
+  ltlv25objective = str("Locktwist XXV\nUnlock 4000 gems") # Level objective
+# Stopping at 25 levels for now
+""" Divider """
+# Accidental/Illegal direct launch
+print("Challenge island #[unknown] - Locktwist")
+noMore = input("This file isn't meant to be executed separately press [ENTER] to close it")
+print("The program has been stopped. If the window is still open, press the close button, or try the --taskkill command")
+""" Divider """
+# End of script
