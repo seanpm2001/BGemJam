f263edf928d1feea83270e05f0823fdf70bee9dc
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-18 17:35:36 -0700

    LEVELS.py
    The first 25 levels for the doomsday mode.

---
+++ BGemJamRoot/Modes/Twist/Challenge/Doomsday/LEVELS.py
@@ -0,0 +1,122 @@
+# Start of script
+"""
+Doomsday mode
+
+Doomsday is a challenge planet for challenge mode, with the objective of destroying doom gems. It was not a part of Bejeweled Twist, I came up with the idea while playing challenge mode.
+
+Unlike the Bejeweled Twist challenge mode, there are more than 7 levels per challenge, and levels are replayable
+"""
+# Levels
+''' Level 1 '''
+def doomsdayLvl1(): # Doomsday challenge level 1
+  ddlv1bo = bool(false) # Unlock status
+  ddlv1lv = int(1) # Level value
+  ddlv1objective = str("Doomsday I\nDestroy 1 doom gem") # Objective
+def doomsdayLvl2(): # Doomsday challenge level 2
+  ddlv2bo = bool(false) # Unlock status
+  ddlv2lv = int(2) # Level value
+  ddlv2objective = str("Doomsday II\nDestroy 1 doom gem without breaking the chain") # Objective
+def doomsdayLvl3(): # Doomsday challenge level 3
+  ddlv3bo = bool(false) # Unlock status
+  ddlv3lv = int(3) # Level value
+  ddlv3objective = str("Doomsday III\nDestroy 1 doom gem without breaking the chain within 5 minutes") # Objective
+def doomsdayLvl4(): # Doomsday challenge level 4
+  ddlv4bo = bool(false) # Unlock status
+  ddlv4lv = int(4) # Level value
+  ddlv4objective = str("Doomsday IV\nDestroy 2 doom gems") # Objective
+def doomsdayLvl5(): # Doomsday challenge level 5
+  ddlv5bo = bool(false) # Unlock status
+  ddlv5lv = int(5) # Level value
+  ddlv5objective = str("Doomsday V\nDestroy 2 doom gems within 50 moves") # Objective
+def doomsdayLvl6(): # Doomsday challenge level 6
+  ddlv6bo = bool(false) # Unlock status
+  ddlv6lv = int(6) # Level value
+  ddlv6objective = str("Doomsday VI\nDestroy 4 doom gems") # Objective
+def doomsdayLvl7(): # Doomsday challenge level 7
+  ddlv7bo = bool(false) # Unlock status
+  ddlv7lv = int(7) # Level value
+  ddlv7objective = str("Doomsday VII\nDestroy 2 doom gems in 1 move") # Objective
+def doomsdayLvl8(): # Doomsday challenge level 8
+  ddlv8bo = bool(false) # Unlock status
+  ddlv8lv = int(8) # Level value
+  ddlv8objective = str("Doomsday VIII\nDestroy 6 doom gems within 5 minutes") # Objective
+def doomsdayLvl9(): # Doomsday challenge level 9
+  ddlv9bo = bool(false) # Unlock status
+  ddlv9lv = int(9) # Level value
+  ddlv9objective = str("Doomsday IX\nDestroy 8 doom gems within 5 minutes") # Objective
+def doomsdayLvl10(): # Doomsday challenge level 10
+  ddlv10bo = bool(false) # Unlock status
+  ddlv10lv = int(10) # Level value
+  ddlv10objective = str("Doomsday X\nDestroy 12 doom gems within 5 minutes") # Objective
+def doomsdayLvl11(): # Doomsday challenge level 11
+  ddlv11bo = bool(false) # Unlock status
+  ddlv11lv = int(11) # Level value
+  ddlv11objective = str("Doomsday XI\nDestroy 12 doom gems within 3 minutes") # Objective
+def doomsdayLvl12(): # Doomsday challenge level 12
+  ddlv12bo = bool(false) # Unlock status
+  ddlv12lv = int(12) # Level value
+  ddlv12objective = str("Doomsday XII\nDestroy 15 doom gems within 5 minutes") # Objective
+def doomsdayLvl13(): # Doomsday challenge level 13
+  ddlv13bo = bool(false) # Unlock status
+  ddlv13lv = int(13) # Level value
+  ddlv13objective = str("Doomsday XIII\nDestroy 25 doom gems") # Objective
+def doomsdayLvl14(): # Doomsday challenge level 14
+  ddlv14bo = bool(false) # Unlock status
+  ddlv14lv = int(14) # Level value
+  ddlv14objective = str("Doomsday XIV\nDestroy 3 doom gems in 1 move") # Objective
+def doomsdayLvl15(): # Doomsday challenge level 15
+  ddlv15bo = bool(false) # Unlock status
+  ddlv15lv = int(15) # Level value
+  ddlv15objective = str("Doomsday XV\nDestroy 3 doom gems in 1 move without using lightning or supernova gems") # Objective
+def doomsdayLvl16(): # Doomsday challenge level 16
+  ddlv16bo = bool(false) # Unlock status
+  ddlv16lv = int(16) # Level value
+  ddlv16objective = str("Doomsday XVI\nDestroy 45 doom gems") # Objective
+def doomsdayLvl17(): # Doomsday challenge level 17
+  ddlv17bo = bool(false) # Unlock status
+  ddlv17lv = int(17) # Level value
+  ddlv17objective = str("Doomsday XVII\nDestroy 60 doom gems") # Objective
+def doomsdayLvl18(): # Doomsday challenge level 18
+  ddlv18bo = bool(false) # Unlock status
+  ddlv18lv = int(18) # Level value
+  ddlv18objective = str("Doomsday XVIII\nDestroy 100 doom gems within 2000 moves") # Objective
+def doomsdayLvl19(): # Doomsday challenge level 19
+  ddlv19bo = bool(false) # Unlock status
+  ddlv19lv = int(19) # Level value
+  ddlv19objective = str("Doomsday XIX\nMatch a doom gem of every color before it activates") # Objective
+def doomsdayLvl20(): # Doomsday challenge level 20
+  ddlv20bo = bool(false) # Unlock status
+  ddlv20lv = int(20) # Level value
+  ddlv20objective = str("Doomsday XX\nHave 6 doom gems on the screen at the same time") # Objective
+def doomsdayLvl21(): # Doomsday challenge level 21
+  ddlv21bo = bool(false) # Unlock status
+  ddlv21lv = int(21) # Level value
+  ddlv21objective = str("Doomsday XXI\nDestroy 150 doom gems") # Objective
+def doomsdayLvl22(): # Doomsday challenge level 22
+  ddlv22bo = bool(false) # Unlock status
+  ddlv22lv = int(22) # Level value
+  ddlv22objective = str("Doomsday XXII\nDestroy 250 doom gems") # Objective
+def doomsdayLvl23(): # Doomsday challenge level 23
+  ddlv23bo = bool(false) # Unlock status
+  ddlv23lv = int(23) # Level value
+  ddlv23objective = str("Doomsday XXIII\nDestroy 3 doom gems with a supernova gem") # Objective
+def doomsdayLvl24(): # Doomsday challenge level 24
+  ddlv24bo = bool(false) # Unlock status
+  ddlv24lv = int(24) # Level value
+  ddlv24objective = str("Doomsday XXIV\nDestroy 350 doom gems") # Objective
+def doomsdayLvl25(): # Doomsday challenge level 25
+  ddlv25bo = bool(false) # Unlock status
+  ddlv25lv = int(25) # Level value
+  ddlv25objective = str("Doomsday XXV\nDestroy 500 doom gems") # Objective
+# Stopping at 25 levels for now
+'''
+Future ideas:
+Survive 100 turns without destroying a doom gem or making a matchless move
+"""
+""" Divider """
+# Accidental/Illegal direct launch
+print("Challenge island #[unknown] - Doomsday")
+noMore = input("This file isn't meant to be executed separately press [ENTER] to close it")
+print("The program has been stopped. If the window is still open, press the close button, or try the --taskkill command")
+""" Divider """
+# End of script
